24f7022b8ab882100435d33a10c879dcc37175a1
RrrandD <experimenting@codeinstitute.net>
2016-10-04 13:18:46 +0100

    Added index and time html templates

--- the_hello_app/views.py
+++ the_hello_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+
+def say_hello(request):
+
+    message = "Hello There!!"
+
+    return render(request, "index.html", {'the_message': message})
+
+
+def get_the_time(request):
+
+    now = datetime.now()
+
+    return render(request, "time.html", {'the_time': now})
+
